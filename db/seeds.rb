764ab7d6338ab65e650238b2410a536595f5cf26
Perry Carbone <perrycarbone@Perrys-MacBook-Pro.local>
2015-09-11 15:40:02 -0400

    update views w/recent_videos method and add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,22 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+Category.create!(
+  name: 'Drama'
+  )
+
+Category.create!(
+  name: 'Horror'
+  )
+
+Category.create!(
+  name: 'Comedy'
+  )
+
+Category.create!(
+  name: 'Romance'
+  )
+
 Video.create!(
   title: 'A Bronx Tale',
   description: 'A boy from the Bronx grows up around the Mafia.',
@@ -52,4 +68,44 @@ Video.create!(
   small_cover_URL: '/tmp/the_godfather_small.jpg',
   large_cover_URL: '/tmp/the_godfather_large.jpg',
   category_id: 2
+  )
+
+Video.create!(
+  title: 'Casino',
+  description: 'The Mafia invades Las Vegas',
+  small_cover_URL: '/tmp/casino_small.jpg',
+  large_cover_URL: '/tmp/casino_large.jpg',
+  category_id: 1
+  )
+
+Video.create!(
+  title: 'Donnie Brasco',
+  description: 'A boy from the Bronx grows up around the Mafia.',
+  small_cover_URL: '/tmp/donnie_brasco_small.jpg',
+  large_cover_URL: '/tmp/donnie_brasco_large.jpg',
+  category_id: 1
+  )
+
+Video.create!(
+  title: 'Goodfellas',
+  description: 'Two men grow up together in NYC and become part of the family.',
+  small_cover_URL: '/tmp/goodfellas_small.jpg',
+  large_cover_URL: '/tmp/goodfellas_large.jpg',
+  category_id: 1
+  )
+
+Video.create!(
+  title: 'Scarface',
+  description: 'Cuban refugee becomes king of the Miami cocaine business.',
+  small_cover_URL: '/tmp/scarface_small.jpg',
+  large_cover_URL: '/tmp/scarface_large.jpg',
+  category_id: 1
+  )
+
+Video.create!(
+  title: 'The Godfather',
+  description: 'The Corleone family fights to retain control of as head of the five families.',
+  small_cover_URL: '/tmp/the_godfather_small.jpg',
+  large_cover_URL: '/tmp/the_godfather_large.jpg',
+  category_id: 1
   )
