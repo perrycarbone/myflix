4e982e1ff7253f98e47ccffeffe45db28469af27
Perry Carbone <perrycarbone@Perrys-MBP.home>
2015-09-17 11:41:34 -0400

    add my_queue page, queue_items table/controller/model and applicable testing.

--- app/models/User.rb
+++ app/models/User.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
+  has_many :queue_items
   has_secure_password validations: false
+  
   validates :full_name, presence: true
   validates :email_address, presence: true
   validates :password, presence: true, on: :create, length: {minimum: 5}
