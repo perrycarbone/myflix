4bd0b90a5bb2db6e5ecff37fb1b95f95f4fec65e
Perry Carbone <perrycarbone@Perrys-MacBook-Pro.local>
2015-09-23 14:22:45 -0400

    add sign in and queue-related feature specs, add launchy gem, modify my-queue button logic on video show page

--- spec/features/user_signs_in_spec.rb
+++ spec/features/user_signs_in_spec.rb
@@ -2,11 +2,8 @@ require 'spec_helper'
 
 feature "User logs in to application" do
   scenario "with correct email address and password" do
-    bob = Fabricate(:user)
-    visit login_path
-    fill_in('Email Address', with: bob.email_address)
-    fill_in('Password', with: bob.password)
-    click_button 'Sign In'
+    bob = Fabricate(:user) 
+    sign_in(bob)
     expect(page).to have_content 'You have successfully logged in.'
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,4 +6,19 @@ describe User do
   it { should validate_presence_of(:full_name) }
   it { should validate_uniqueness_of(:email_address) }
   it { should have_many(:queue_items).order('position') }
+
+  describe "#queued_video?" do
+    it "returns true when the user has the video queued in their queue" do
+      user = Fabricate(:user)
+      video = Fabricate(:video)
+      queue_item = Fabricate(:queue_item, user: user, video: video)
+      user.queued_video?(video).should be_truthy
+    end
+
+    it "returns false when the user does not have the video queued in their queue" do
+      user = Fabricate(:user)
+      video = Fabricate(:video)
+      user.queued_video?(video).should be_falsey
+    end
+  end
 end
