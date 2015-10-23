08f1ba7514592b4658f89b323b9b6d850299d7fc
Perry Carbone <perrycarbone@Perrys-MacBook-Pro.local>
2015-10-23 09:45:36 -0400

    refactor video model for tokenable

--- app/models/Video.rb
+++ app/models/Video.rb
@@ -1,10 +1,10 @@
 class Video < ActiveRecord::Base
+  include Tokenable
+
   belongs_to :category
   belongs_to :user
   has_many :reviews, -> { order 'created_at DESC' }
   validates_presence_of :title, :description
-
-  before_create :generate_token
 
   def self.search_by_title(search_term)
     return [] if search_term.blank?
@@ -13,9 +13,5 @@ class Video < ActiveRecord::Base
 
   def to_param
     token
-  end
-
-  def generate_token
-    self.token = SecureRandom.urlsafe_base64
   end
 end
