cc45e7166279dc24813e34ab6af2af2a75893ba5
Perry Carbone <perrycarbone@Perrys-MacBook-Pro.local>
2015-10-27 14:57:44 -0400

    implement bug fix

--- spec/support/macros.rb
+++ spec/support/macros.rb
@@ -2,7 +2,7 @@ def set_current_user(user=nil)
   session[:user_id] = (user || Fabricate(:user)).id
 end
 
-def sign_in(a_user=nil)
+def sign_in(user=nil)
   user ||= Fabricate(:user)
   visit login_path
   fill_in('Email Address', with: user.email_address)
