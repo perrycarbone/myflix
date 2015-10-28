0cec02ff7dcbd622b4338991bb7cdfaad8a94f40
Perry Carbone <perrycarbone@Perrys-MacBook-Pro.local>
2015-10-28 11:42:35 -0400

    add admin video create feature

--- app/controllers/admin/videos_controller.rb
+++ app/controllers/admin/videos_controller.rb
@@ -6,6 +6,18 @@ class Admin::VideosController < ApplicationController
     @video = Video.new
   end
 
+  def create
+    @video = Video.create(video_params)
+
+    if @video.save
+      flash[:success] = "The video has been added."
+      redirect_to new_admin_video_path
+    else
+      flash.now[:danger] = "Please fill out all required fields."
+      render :new
+    end
+  end
+
   private
 
   def require_admin
@@ -13,5 +25,9 @@ class Admin::VideosController < ApplicationController
       flash[:danger] = "You do not have permission to do that."
       redirect_to home_path
     end
+  end
+
+  def video_params
+    params.require(:video).permit(:title, :category_id, :description)
   end
 end
--- spec/controllers/admin/videos_controller_spec.rb
+++ spec/controllers/admin/videos_controller_spec.rb
@@ -6,16 +6,14 @@ describe Admin::VideosController do
       let(:action) { get :new }
     end
 
+    it_behaves_like "requires admin" do
+      let(:action) { get :new }
+    end
+
     it "sets @video to a new video" do
       set_current_admin
       get :new
       expect(assigns(:video)).to be_a_new(Video)
-    end
-
-    it "redirects a regular user to home path" do
-      set_current_user
-      get :new
-      expect(response).to redirect_to home_path
     end
 
     it "sets the flash error message for regular user" do
@@ -23,5 +21,70 @@ describe Admin::VideosController do
       get :new
       expect(flash[:danger]).to be_present
     end
+  end
+
+  describe "POST create" do
+    it_behaves_like "requires sign in" do
+      let(:action) { post :create }
+    end
+
+    it_behaves_like "requires admin" do
+      let(:action) { post :create }
+    end
+
+    context "with valid input" do
+      it "redirects to the add new video page" do
+        set_current_admin
+        category = Fabricate(:category)
+        post :create, video: { title: 'goodfellas', category_id: category.id, description: "Lots of blood!" }
+        expect(response).to redirect_to new_admin_video_path
+      end
+
+      it "creates a video" do
+        set_current_admin
+        category = Fabricate(:category)
+        post :create, video: { title: 'goodfellas', category_id: category.id, description: "Lots of blood!" }
+        expect(category.videos.count).to eq(1)
+      end
+
+      it "sets the flash success message" do
+        set_current_admin
+        category = Fabricate(:category)
+        post :create, video: { title: 'goodfellas', category_id: category.id, description: "Lots of blood!" }
+        expect(flash[:success]).to be_present
+      end
+    end
+
+    context "with invalid input" do
+      it "does not create the video" do
+        set_current_admin
+        category = Fabricate(:category)
+        post :create, video: { title: 'goodfellas' }
+        expect(category.videos.count).to eq(0)
+      end
+
+      it "renders the video new template" do
+        set_current_admin
+        category = Fabricate(:category)
+        post :create, video: { title: 'goodfellas' }
+        expect(response).to render_template :new
+      end
+
+      it "sets @video" do
+        set_current_admin
+        category = Fabricate(:category)
+        post :create, video: { title: 'goodfellas' }
+        expect(assigns(:video)).to be_present
+      end
+
+      it "sets the flash error message" do
+        set_current_admin
+        category = Fabricate(:category)
+        post :create, video: { title: 'goodfellas' }
+        expect(flash[:danger]).to be_present
+      end
+    end
+
+
   end
 end
