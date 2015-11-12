0fc0094ea7f0d3ef5f32daf030f230a794d341d3
Perry Carbone <perrycarb@gmail.com>
2015-11-12 16:21:10 -0500

    add search by video title and description

--- app/models/video.rb
+++ app/models/video.rb
@@ -19,4 +19,21 @@ class Video < ActiveRecord::Base
   def rating
     reviews.average(:rating).round(1) if reviews.average(:rating)
   end
+
+  def self.search(query)
+    search_definition = {
+      query: {
+        multi_match: {
+          query: query,
+          fields: ['title', 'description'],
+          operator: 'and'
+        }
+      }
+    }
+    __elasticsearch__.search(search_definition)
+  end
+
+  def as_indexed_json(options={})
+    as_json(only: [:title, :description])
+  end
 end
